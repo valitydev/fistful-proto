03f620b54774f251a05ccab93146a425775ec018
dinama <dinama@users.noreply.github.com>
2020-11-18 10:09:15 +0300

    FF-237: ResourceToken serialization (#133)

---
+++ proto/resource_token.thrift
@@ -0,0 +1,29 @@
+/**
+ * Структуры для сериализации токена платёжного ресурса
+ */
+
+namespace java com.rbkmoney.fistful.resource_token
+namespace erlang rst
+
+include "base.thrift"
+
+/**
+ *  Токен пользовательского платёжного ресурса. Токен содержит чувствительные данные, которые сериализуются
+ *  в thrift-binary и шифруются перед отправкой пользователю.  Токен может иметь срок действия, по истечении которого
+ *  становится недействительным.
+ */
+struct ResourceToken {
+    1: required ResourcePayload payload
+    2: optional base.Timestamp valid_until
+}
+
+/**
+ *  Данные платёжного ресурса
+ */
+union ResourcePayload {
+    1: BankCardPayload bank_card_payload
+}
+
+struct BankCardPayload {
+    1: required base.BankCard bank_card
+}
